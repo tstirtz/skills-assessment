590868a6a386badab23ffd24275f2b77ff27887e
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-06-21 21:35:14 -0700

    Format docs with prettier

--- contact-list.js
+++ contact-list.js
@@ -1,12 +1,15 @@
-window.onload = function(){
-  document.getElementsByTagName('select')[0].addEventListener('change', function(event){
-    if(this.value === 'phone-number'){
-      document.getElementById('contact-list-emails').className = 'hidden';
-      document.getElementById('contact-list-phones').className = 'contact-list-items';
-    }else if(this.value === 'email-address'){
-      document.getElementById('contact-list-emails').className = 'contact-list-items';
-      document.getElementById('contact-list-phones').className = 'hidden';
-    }
-  });
-}
-
+window.onload = function() {
+  document
+    .getElementsByTagName("select")[0]
+    .addEventListener("change", function(event) {
+      if (this.value === "phone-number") {
+        document.getElementById("contact-list-emails").className = "hidden";
+        document.getElementById("contact-list-phones").className =
+          "contact-list-items";
+      } else if (this.value === "email-address") {
+        document.getElementById("contact-list-emails").className =
+          "contact-list-items";
+        document.getElementById("contact-list-phones").className = "hidden";
+      }
+    });
+};
